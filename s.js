39af7bbda9263a1ba1d0acfca4bc4be134d8bd22
jstsevval <157425704+jstsevval@users.noreply.github.com>
2024-01-23 14:53:48 +0300

    Add files via upload

---
+++ s.js
@@ -0,0 +1,21 @@
+
+function myFunction(element, clr) {
+    element.style.color = clr;
+}
+
+
+
+function enlargeImg(img) {
+    img.style.transform = "scale(2)";
+    img.style.transition =
+      "transform 0.25s ease";
+}
+
+
+
+
+
+
+
+
+
